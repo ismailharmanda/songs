cf59d690144ca3e99c45cceea6b78e439a956177
ismail harmanda <iharmanda@hotmail.com>
2021-06-28 23:52:40 +0300

    Provied has been wiredup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 const songsReducer = () => {
   return [
     { title: "No Scrubs", duration: "4.05" },
@@ -10,3 +11,8 @@ const songsReducer = () => {
 const selectedSongReducer = (selectedSong = null, action) => {
   return action.type === "SONG_SELECTED" ? action.payload : selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
